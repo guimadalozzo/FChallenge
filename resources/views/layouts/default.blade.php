3bca007f4945042abb4328e52a3d88670b71303e
Guilherme Madalozzo <guimadalozzo@gmail.com>
2022-09-17 18:49:10 -0300

    Feat: adição do SweetAlert2 e Select2 no template do front-ed

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,15 +9,13 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 @include('includes.header')
             
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    @yield('content')
-                </div>
+                @yield('content')
             
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     @include('includes.footer')
                 </div>
             </div>
         </div>
+        @stack('js')
     </body>
 </html>
--- resources/views/pages/agendamento.blade.php
+++ resources/views/pages/agendamento.blade.php
@@ -2,63 +2,103 @@
 
 @section('content')
 
+<style>
+    .grid-container {
+        display: grid;
+        grid-template-columns: 0.7fr 2fr;
+    }
+    .grid-item {
+      padding-top: 10px;
+      text-align: center;
+    }
+</style>
 
-@foreach($especialidades as $item)
-    <li>{{$item->nome}}</li>
-@endforeach
-
-
-<div class="grid grid-cols-1 md:grid-cols-2">
-    <div class="p-6">
-        <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
+<section class="content"> 
+    <input type="hidden" id="storedCtrl" name="storedCtrl" value={{$stored}}>
+    <div class="card card-default">
+        <div class="card-header">
+            <h3 class="card-title">Agendamento de Consulta</h3>
         </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+        
+        <div class="card-body">
+            <div class="row">
+                <label class="label"><b>Selecione uma Especialidade</b></label>
+            </div>
+            <div class="row">
+                <form action="{{route('agendamento.list')}}" method="POST">
+                    @csrf
+                    <div class="input-group">
+                        <select class="especialidades-dropdown" name="especialidade" id="especialidade" tabindex="-1" aria-hidden="true">
+                            @foreach($especialidades as $item)
+                                @if($spec == $item->especialidade_id)
+                                    <option selected value={{$item->especialidade_id}}>{{$item->nome}}</option>
+                                @else
+                                    <option value={{$item->especialidade_id}}>{{$item->nome}}</option>
+                                @endif
+                            @endforeach
+                        </select>  
+                        <span class="input-group-append">
+                            <button type="submit" class="btn btn-sm btn-danger">Buscar</button>
+                        </span>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-        <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a></div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+    @if(isset($especialistas) and !empty($especialistas))
+        <br>
+        <div class="card card-default" id="specs-list">
+            <div class="card-header">
+                <h3 class="card-title">Especialistas encontrados</h3>
+            </div>
+            
+            <div class="card-body">
+                <div class="row">
+                    @foreach($especialistas as $especialista)
+                        <div class="col-md-6" style="padding-top: 5px; padding-bottom: 5px">
+                            <div class="card card-widget widget-user">
+                                <form action="{{route('agendamento.create')}}" method="POST">
+                                    @csrf
+                                <div class="grid-container">
+                                    <input type="hidden" id="specId" name="specId" value={{$spec}}>
+                                    <input type="hidden" id="profId" name="profId" value={{$especialista->profissional_id}}>
+                                    <div class="grid-item">
+                                        <img style="height:80px; width:80px; border-radius: 50%; padding-bottom: 5px" src="{{ $especialista->foto ?: asset('images/spec-default.png') }}" alt="User Avatar">
+                                    </div>
+                                    <div class="grid-item">
+                                        <h4 class="widget-user-username">{{ $especialista->nome }}</h4>
+                                        <h6 class="widget-user-desc">{{ $especialista->conselho }} {{ $especialista->rqe }}</h6>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <button type="submit" class="btn btn-success">Agendar</button>
+                                    </div>
+                                </div>
+                            </form>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
-        <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Laravel News</a></div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel News is a community driven portal and newsletter aggregating all of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials.
-            </div>
-        </div>
-    </div>
-
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-        <div class="flex items-center">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</div>
-        </div>
-
-        <div class="ml-12">
-            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                Laravel's robust library of first-party tools and libraries, such as <a href="https://forge.laravel.com" class="underline">Forge</a>, <a href="https://vapor.laravel.com" class="underline">Vapor</a>, <a href="https://nova.laravel.com" class="underline">Nova</a>, and <a href="https://envoyer.io" class="underline">Envoyer</a> help you take your projects to the next level. Pair them with powerful open source libraries like <a href="https://laravel.com/docs/billing" class="underline">Cashier</a>, <a href="https://laravel.com/docs/dusk" class="underline">Dusk</a>, <a href="https://laravel.com/docs/broadcasting" class="underline">Echo</a>, <a href="https://laravel.com/docs/horizon" class="underline">Horizon</a>, <a href="https://laravel.com/docs/sanctum" class="underline">Sanctum</a>, <a href="https://laravel.com/docs/telescope" class="underline">Telescope</a>, and more.
-            </div>
-        </div>
-    </div>
-</div>
+    @endif
+</section>
 @stop
+
+@push('js')
+<script>
+    $(document).ready(function() {
+        $('.especialidades-dropdown').select2();
+        var swalCtrl = document.getElementById("storedCtrl").value;
+        if (swalCtrl)
+            Swal.fire({
+                title: 'Sucesso!',
+                text: 'Solicitação de horário registrada com sucesso!',
+                icon: 'success',
+                confirmButtonText: 'Ok!'
+            })
+    });
+</script>
+@endpush
