bdcc5051ec96d04aa0c464a821ed0689633e857e
Guilherme Madalozzo <guimadalozzo@gmail.com>
2022-09-17 11:01:24 -0300

    Feat: montada arquitetura de diretórios e arquivos para padronização de erros e variáveis de ambiente

---
+++ config/error.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'list_specialties' => 'Ocorreram erros ao buscar as especialidades.',
+];
---
+++ config/feegow.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_path'  => env('API_FEEGOW'),
+    'api_token' => env('TOKEN_FEEGOW'),
+];
